ef4f88b339e963764a4adfc5114fc960ef6bfb57
Rivi03 <riosschrodinger@gmail.com>
2025-08-23 23:12:04 -0600

    adicion de todo alv

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Home from './pages/HomePage'
+import AboutUs from './pages/AboutUsPage'
+import Login from './pages/LoginPage'
+
+function App() {
+  return (
+    <Login/>
+  )
+}
+
+export default App
