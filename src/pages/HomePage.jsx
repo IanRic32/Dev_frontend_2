332c0200157479f38db7f89de136c878404cbfcd
EsmeGonzalez1758 <imeldaeg2017@gmail.com>
2025-08-24 00:38:35 -0600

    Feature/HomePage:categories section in HomePage with initial design and links

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,3 +2,36 @@ import React from 'react';
 import Footer from '../components/Footer';
 import '../styles/Home.css';
 
+function HomePage() {
+  const categorias = [
+    { link: "#", categoria: "Ropa" },
+    { link: "#", categoria: "Tecnología" },
+    { link: "#", categoria: "Decoración y Hogar" },
+    { link: "#", categoria: "Deportes" }
+  ];
+
+  return (
+    <>
+      <section id="categorias" className="mis-categorias seccion-clara d-flex flex-column align-items-center justify-content-center">
+        <div className="container text-center categorias-contenedor">
+          <div className="row">
+            {categorias.map((espacio, index) => (
+              <div key={index} className="col-12 col-md-6 mb-4">
+                <a href={espacio.link} target="_blank" rel="noopener noreferrer">
+                  <div className={`proyecto ${espacio.categoria.replace(/\s+/g, '-')}`}>
+                    <div className="overlay">
+                      <p>{espacio.categoria}</p>
+                    </div>
+                  </div>
+                </a>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+      <Footer />
+    </>
+  );
+}
+
+export default HomePage;
